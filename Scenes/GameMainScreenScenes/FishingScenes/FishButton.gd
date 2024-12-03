79c4d36dc45f9a6781573ab7f78db28b1d8fd746
Martins Davis Bernhards <martinsdavisbernhards@gmail.com>
2024-12-03 08:30:51 +0100

    Small fish update

--- Scenes/GameMainScreenScenes/FishingScenes/FishButton.gd
+++ Scenes/GameMainScreenScenes/FishingScenes/FishButton.gd
@@ -8,7 +8,7 @@ var RNG = RandomNumberGenerator.new()
 var VerticalSpeed = 1
 var HorizontalSpeed = 1
 
-var Fish
+var Fish : Fishing.FishObject
 
 @export var FishType: Fishing.FishEnum = Fishing.FishEnum.Boot :
 	set(value):
@@ -22,29 +22,32 @@ func _ready() -> void:
 	pass
 
 func _process(delta: float) -> void:
+	move(delta)
+
+func move(delta):
 	if Up:
-		position.y -= VerticalSpeed * delta * 144
+		position.y -= VerticalSpeed * delta * 144 * Fish.Speed
 	else:
-		position.y += VerticalSpeed * delta * 144
+		position.y += VerticalSpeed * delta * 144 * Fish.Speed
 
 	if Right:
-		position.x += HorizontalSpeed * delta * 144
+		position.x += HorizontalSpeed * delta * 144 * Fish.Speed
 	else:
-		position.x -= HorizontalSpeed * delta * 144
+		position.x -= HorizontalSpeed * delta * 144 * Fish.Speed
 		
-	if position.x > 970 and Right:
+	if position.x > 940 and Right: # Need to account for size
 		Right = false
-	elif position.x < 3 and !Right:
+	elif position.x < 25 and !Right:
 		Right = true
 		
-	if position.y < 3 and Up:
+	if position.y < 25 and Up:
 		Up = false
-	elif position.y > 730 and !Up:
+	elif position.y > 760 and !Up:  # Need to account for size
 		Up = true
 
 func _on_button_down() -> void:
 	queue_free()
 
 func _on_change_direction_timer_timeout() -> void:
-	VerticalSpeed = RNG.randf_range(0.5, 1.5)
-	HorizontalSpeed = RNG.randf_range(0.5, 1.5)
+	VerticalSpeed = RNG.randf_range(0.5 * Fish.Angle, 1.5 * Fish.Angle)
+	HorizontalSpeed = RNG.randf_range(0.5 * Fish.Angle, 1.5 * Fish.Angle)
