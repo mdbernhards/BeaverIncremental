1b71b2d7b39468838546f6f37cc5031ee9fe5ad3
Martins Davis Bernhards <martinsdavisbernhards@gmail.com>
2024-12-20 11:45:07 +0100

    Dam construction logic

---
+++ Scenes/GameMainScreenScenes/GameScenes/DamSection/constructionItem/construction_resource.gd
@@ -0,0 +1,7 @@
+extends ColorRect
+
+func _ready() -> void:
+	pass
+
+func _process(delta: float) -> void:
+	pass
--- singletons/constantData/Dams.gd
+++ singletons/constantData/Dams.gd
@@ -27,7 +27,7 @@ var Dams = {
 		DamParts.First : {
 			"Name" : "Gathering a team of Beavers",
 			"Price" : {
-				"OakCost" : 1, "AppleCost" : 1, "MapleCost" : 0, "BirchCost" : 0, "SpruceCost" : 0,
+				"OakCost" : 100, "AppleCost" : 100, "MapleCost" : 0, "BirchCost" : 0, "SpruceCost" : 0,
 				"ChestnutCost" : 0, "CherryCost" : 0, "AshCost" : 0, "CedarCost" : 0, "MahoganyCost" : 0,
 				"EbonyCost" : 0, "DogwoodCost" : 0, "RosewoodCost" : 0, "Ghost GumCost" : 0, "DragonwoodCost" : 0,
 			},
@@ -36,7 +36,7 @@ var Dams = {
 		DamParts.Second : {
 			"Name" : "Building Foundations",
 			"Price" : {
-				"OakCost" : 1, "AppleCost" : 1, "MapleCost" : 0, "BirchCost" : 0, "SpruceCost" : 0,
+				"OakCost" : 100, "AppleCost" : 100, "MapleCost" : 0, "BirchCost" : 0, "SpruceCost" : 0,
 				"ChestnutCost" : 0, "CherryCost" : 0, "AshCost" : 0, "CedarCost" : 0, "MahoganyCost" : 0,
 				"EbonyCost" : 0, "DogwoodCost" : 0, "RosewoodCost" : 0, "Ghost GumCost" : 0, "DragonwoodCost" : 0,
 			},
@@ -45,7 +45,7 @@ var Dams = {
 		DamParts.Third : {
 			"Name" : "Careful Wood Stacking",
 			"Price" : {
-				"OakCost" : 1, "AppleCost" : 1, "MapleCost" : 0, "BirchCost" : 0, "SpruceCost" : 0,
+				"OakCost" : 100, "AppleCost" : 100, "MapleCost" : 0, "BirchCost" : 0, "SpruceCost" : 0,
 				"ChestnutCost" : 0, "CherryCost" : 0, "AshCost" : 0, "CedarCost" : 0, "MahoganyCost" : 0,
 				"EbonyCost" : 0, "DogwoodCost" : 0, "RosewoodCost" : 0, "Ghost GumCost" : 0, "DragonwoodCost" : 0,
 			},
@@ -54,7 +54,7 @@ var Dams = {
 		DamParts.Fourth : {
 			"Name" : "Patching leaks",
 			"Price" : {
-				"OakCost" : 1, "AppleCost" : 1, "MapleCost" : 0, "BirchCost" : 0, "SpruceCost" : 0,
+				"OakCost" : 100, "AppleCost" : 100, "MapleCost" : 0, "BirchCost" : 0, "SpruceCost" : 0,
 				"ChestnutCost" : 0, "CherryCost" : 0, "AshCost" : 0, "CedarCost" : 0, "MahoganyCost" : 0,
 				"EbonyCost" : 0, "DogwoodCost" : 0, "RosewoodCost" : 0, "Ghost GumCost" : 0, "DragonwoodCost" : 0,
 			},
@@ -65,14 +65,14 @@ var Dams = {
 		"Name" : "Medium Dam",
 		"Effect" : "effect",
 		"StartingPrice" : {
-			"OakCost" : 1, "AppleCost" : 1, "MapleCost" : 0, "BirchCost" : 0, "SpruceCost" : 0,
+			"OakCost" : 2, "AppleCost" : 2, "MapleCost" : 0, "BirchCost" : 0, "SpruceCost" : 0,
 			"ChestnutCost" : 0, "CherryCost" : 0, "AshCost" : 0, "CedarCost" : 0, "MahoganyCost" : 0,
 			"EbonyCost" : 0, "DogwoodCost" : 0, "RosewoodCost" : 0, "Ghost GumCost" : 0, "DragonwoodCost" : 0,
 		},
 		DamParts.First : {
 			"Name" : "Site Eppraisal",
 			"Price" : {
-				"OakCost" : 1, "AppleCost" : 1, "MapleCost" : 0, "BirchCost" : 0, "SpruceCost" : 0,
+				"OakCost" : 200, "AppleCost" : 200, "MapleCost" : 0, "BirchCost" : 0, "SpruceCost" : 0,
 				"ChestnutCost" : 0, "CherryCost" : 0, "AshCost" : 0, "CedarCost" : 0, "MahoganyCost" : 0,
 				"EbonyCost" : 0, "DogwoodCost" : 0, "RosewoodCost" : 0, "Ghost GumCost" : 0, "DragonwoodCost" : 0,
 			},
@@ -81,7 +81,7 @@ var Dams = {
 		DamParts.Second : {
 			"Name" : "Careful Planing",
 			"Price" : {
-				"OakCost" : 1, "AppleCost" : 1, "MapleCost" : 0, "BirchCost" : 0, "SpruceCost" : 0,
+				"OakCost" : 200, "AppleCost" : 200, "MapleCost" : 0, "BirchCost" : 0, "SpruceCost" : 0,
 				"ChestnutCost" : 0, "CherryCost" : 0, "AshCost" : 0, "CedarCost" : 0, "MahoganyCost" : 0,
 				"EbonyCost" : 0, "DogwoodCost" : 0, "RosewoodCost" : 0, "Ghost GumCost" : 0, "DragonwoodCost" : 0,
 			},
@@ -90,7 +90,7 @@ var Dams = {
 		DamParts.Third : {
 			"Name" : "A Careful Excecution Of The Plan",
 			"Price" : {
-				"OakCost" : 1, "AppleCost" : 1, "MapleCost" : 0, "BirchCost" : 0, "SpruceCost" : 0,
+				"OakCost" : 200, "AppleCost" : 200, "MapleCost" : 0, "BirchCost" : 0, "SpruceCost" : 0,
 				"ChestnutCost" : 0, "CherryCost" : 0, "AshCost" : 0, "CedarCost" : 0, "MahoganyCost" : 0,
 				"EbonyCost" : 0, "DogwoodCost" : 0, "RosewoodCost" : 0, "Ghost GumCost" : 0, "DragonwoodCost" : 0,
 			},
@@ -99,7 +99,7 @@ var Dams = {
 		DamParts.Fourth : {
 			"Name" : "Finishing Touches",
 			"Price" : {
-				"OakCost" : 1, "AppleCost" : 1, "MapleCost" : 0, "BirchCost" : 0, "SpruceCost" : 0,
+				"OakCost" : 200, "AppleCost" : 200, "MapleCost" : 0, "BirchCost" : 0, "SpruceCost" : 0,
 				"ChestnutCost" : 0, "CherryCost" : 0, "AshCost" : 0, "CedarCost" : 0, "MahoganyCost" : 0,
 				"EbonyCost" : 0, "DogwoodCost" : 0, "RosewoodCost" : 0, "Ghost GumCost" : 0, "DragonwoodCost" : 0,
 			},
@@ -110,14 +110,14 @@ var Dams = {
 		"Name" : "Big Dam",
 		"Effect" : "effect",
 		"StartingPrice" : {
-			"OakCost" : 1, "AppleCost" : 1, "MapleCost" : 0, "BirchCost" : 0, "SpruceCost" : 0,
+			"OakCost" : 3, "AppleCost" : 3, "MapleCost" : 0, "BirchCost" : 0, "SpruceCost" : 0,
 			"ChestnutCost" : 0, "CherryCost" : 0, "AshCost" : 0, "CedarCost" : 0, "MahoganyCost" : 0,
 			"EbonyCost" : 0, "DogwoodCost" : 0, "RosewoodCost" : 0, "Ghost GumCost" : 0, "DragonwoodCost" : 0,
 		},
 		DamParts.First : {
 			"Name" : "Fund Raising",
 			"Price" : {
-				"OakCost" : 1, "AppleCost" : 1, "MapleCost" : 0, "BirchCost" : 0, "SpruceCost" : 0,
+				"OakCost" : 300, "AppleCost" : 300, "MapleCost" : 0, "BirchCost" : 0, "SpruceCost" : 0,
 				"ChestnutCost" : 0, "CherryCost" : 0, "AshCost" : 0, "CedarCost" : 0, "MahoganyCost" : 0,
 				"EbonyCost" : 0, "DogwoodCost" : 0, "RosewoodCost" : 0, "Ghost GumCost" : 0, "DragonwoodCost" : 0,
 			},
@@ -126,7 +126,7 @@ var Dams = {
 		DamParts.Second : {
 			"Name" : "Creating Logistics Lines",
 			"Price" : {
-				"OakCost" : 1, "AppleCost" : 1, "MapleCost" : 0, "BirchCost" : 0, "SpruceCost" : 0,
+				"OakCost" : 300, "AppleCost" : 300, "MapleCost" : 0, "BirchCost" : 0, "SpruceCost" : 0,
 				"ChestnutCost" : 0, "CherryCost" : 0, "AshCost" : 0, "CedarCost" : 0, "MahoganyCost" : 0,
 				"EbonyCost" : 0, "DogwoodCost" : 0, "RosewoodCost" : 0, "Ghost GumCost" : 0, "DragonwoodCost" : 0,
 			},
@@ -135,7 +135,7 @@ var Dams = {
 		DamParts.Third : {
 			"Name" : "Crazy Fast Construction",
 			"Price" : {
-				"OakCost" : 1, "AppleCost" : 1, "MapleCost" : 0, "BirchCost" : 0, "SpruceCost" : 0,
+				"OakCost" : 300, "AppleCost" : 300, "MapleCost" : 0, "BirchCost" : 0, "SpruceCost" : 0,
 				"ChestnutCost" : 0, "CherryCost" : 0, "AshCost" : 0, "CedarCost" : 0, "MahoganyCost" : 0,
 				"EbonyCost" : 0, "DogwoodCost" : 0, "RosewoodCost" : 0, "Ghost GumCost" : 0, "DragonwoodCost" : 0,
 			},
@@ -144,7 +144,7 @@ var Dams = {
 		DamParts.Fourth : {
 			"Name" : "Weight Testing",
 			"Price" : {
-				"OakCost" : 1, "AppleCost" : 1, "MapleCost" : 0, "BirchCost" : 0, "SpruceCost" : 0,
+				"OakCost" : 300, "AppleCost" : 300, "MapleCost" : 0, "BirchCost" : 0, "SpruceCost" : 0,
 				"ChestnutCost" : 0, "CherryCost" : 0, "AshCost" : 0, "CedarCost" : 0, "MahoganyCost" : 0,
 				"EbonyCost" : 0, "DogwoodCost" : 0, "RosewoodCost" : 0, "Ghost GumCost" : 0, "DragonwoodCost" : 0,
 			},
@@ -155,14 +155,14 @@ var Dams = {
 		"Name" : "Giant Dam",
 		"Effect" : "effect",
 		"StartingPrice" : {
-			"OakCost" : 1, "AppleCost" : 1, "MapleCost" : 0, "BirchCost" : 0, "SpruceCost" : 0,
+			"OakCost" : 4, "AppleCost" : 4, "MapleCost" : 0, "BirchCost" : 0, "SpruceCost" : 0,
 			"ChestnutCost" : 0, "CherryCost" : 0, "AshCost" : 0, "CedarCost" : 0, "MahoganyCost" : 0,
 			"EbonyCost" : 0, "DogwoodCost" : 0, "RosewoodCost" : 0, "Ghost GumCost" : 0, "DragonwoodCost" : 0,
 		},
 		DamParts.First : {
 			"Name" : "Attracting Investors",
 			"Price" : {
-				"OakCost" : 1, "AppleCost" : 1, "MapleCost" : 0, "BirchCost" : 0, "SpruceCost" : 0,
+				"OakCost" : 400, "AppleCost" : 400, "MapleCost" : 0, "BirchCost" : 0, "SpruceCost" : 0,
 				"ChestnutCost" : 0, "CherryCost" : 0, "AshCost" : 0, "CedarCost" : 0, "MahoganyCost" : 0,
 				"EbonyCost" : 0, "DogwoodCost" : 0, "RosewoodCost" : 0, "Ghost GumCost" : 0, "DragonwoodCost" : 0,
 			},
@@ -171,7 +171,7 @@ var Dams = {
 		DamParts.Second : {
 			"Name" : "Environmental Impact Studies",
 			"Price" : {
-				"OakCost" : 1, "AppleCost" : 1, "MapleCost" : 0, "BirchCost" : 0, "SpruceCost" : 0,
+				"OakCost" : 400, "AppleCost" : 400, "MapleCost" : 0, "BirchCost" : 0, "SpruceCost" : 0,
 				"ChestnutCost" : 0, "CherryCost" : 0, "AshCost" : 0, "CedarCost" : 0, "MahoganyCost" : 0,
 				"EbonyCost" : 0, "DogwoodCost" : 0, "RosewoodCost" : 0, "Ghost GumCost" : 0, "DragonwoodCost" : 0,
 			},
@@ -180,7 +180,7 @@ var Dams = {
 		DamParts.Third : {
 			"Name" : "Architecture",
 			"Price" : {
-				"OakCost" : 1, "AppleCost" : 1, "MapleCost" : 0, "BirchCost" : 0, "SpruceCost" : 0,
+				"OakCost" : 400, "AppleCost" : 400, "MapleCost" : 0, "BirchCost" : 0, "SpruceCost" : 0,
 				"ChestnutCost" : 0, "CherryCost" : 0, "AshCost" : 0, "CedarCost" : 0, "MahoganyCost" : 0,
 				"EbonyCost" : 0, "DogwoodCost" : 0, "RosewoodCost" : 0, "Ghost GumCost" : 0, "DragonwoodCost" : 0,
 			},
@@ -189,7 +189,7 @@ var Dams = {
 		DamParts.Fourth : {
 			"Name" : "Prepairing The Grand Opening",
 			"Price" : {
-				"OakCost" : 1, "AppleCost" : 1, "MapleCost" : 0, "BirchCost" : 0, "SpruceCost" : 0,
+				"OakCost" : 400, "AppleCost" : 400, "MapleCost" : 0, "BirchCost" : 0, "SpruceCost" : 0,
 				"ChestnutCost" : 0, "CherryCost" : 0, "AshCost" : 0, "CedarCost" : 0, "MahoganyCost" : 0,
 				"EbonyCost" : 0, "DogwoodCost" : 0, "RosewoodCost" : 0, "Ghost GumCost" : 0, "DragonwoodCost" : 0,
 			},
@@ -200,14 +200,14 @@ var Dams = {
 		"Name" : "Mega Dam",
 		"Effect" : "effect",
 		"StartingPrice" : {
-			"OakCost" : 1, "AppleCost" : 1, "MapleCost" : 0, "BirchCost" : 0, "SpruceCost" : 0,
+			"OakCost" : 5, "AppleCost" : 5, "MapleCost" : 0, "BirchCost" : 0, "SpruceCost" : 0,
 			"ChestnutCost" : 0, "CherryCost" : 0, "AshCost" : 0, "CedarCost" : 0, "MahoganyCost" : 0,
 			"EbonyCost" : 0, "DogwoodCost" : 0, "RosewoodCost" : 0, "Ghost GumCost" : 0, "DragonwoodCost" : 0,
 		},
 		DamParts.First : {
 			"Name" : "Buying Oligarchs",
 			"Price" : {
-				"OakCost" : 1, "AppleCost" : 1, "MapleCost" : 0, "BirchCost" : 0, "SpruceCost" : 0,
+				"OakCost" : 500, "AppleCost" : 500, "MapleCost" : 0, "BirchCost" : 0, "SpruceCost" : 0,
 				"ChestnutCost" : 0, "CherryCost" : 0, "AshCost" : 0, "CedarCost" : 0, "MahoganyCost" : 0,
 				"EbonyCost" : 0, "DogwoodCost" : 0, "RosewoodCost" : 0, "Ghost GumCost" : 0, "DragonwoodCost" : 0,
 			},
@@ -216,7 +216,7 @@ var Dams = {
 		DamParts.Second : {
 			"Name" : "Over Throwing The Government",
 			"Price" : {
-				"OakCost" : 1, "AppleCost" : 1, "MapleCost" : 0, "BirchCost" : 0, "SpruceCost" : 0,
+				"OakCost" : 500, "AppleCost" : 500, "MapleCost" : 0, "BirchCost" : 0, "SpruceCost" : 0,
 				"ChestnutCost" : 0, "CherryCost" : 0, "AshCost" : 0, "CedarCost" : 0, "MahoganyCost" : 0,
 				"EbonyCost" : 0, "DogwoodCost" : 0, "RosewoodCost" : 0, "Ghost GumCost" : 0, "DragonwoodCost" : 0,
 			},
@@ -225,7 +225,7 @@ var Dams = {
 		DamParts.Third : {
 			"Name" : "Mass Subjugation",
 			"Price" : {
-				"OakCost" : 1, "AppleCost" : 1, "MapleCost" : 0, "BirchCost" : 0, "SpruceCost" : 0,
+				"OakCost" : 500, "AppleCost" : 500, "MapleCost" : 0, "BirchCost" : 0, "SpruceCost" : 0,
 				"ChestnutCost" : 0, "CherryCost" : 0, "AshCost" : 0, "CedarCost" : 0, "MahoganyCost" : 0,
 				"EbonyCost" : 0, "DogwoodCost" : 0, "RosewoodCost" : 0, "Ghost GumCost" : 0, "DragonwoodCost" : 0,
 			},
@@ -234,7 +234,7 @@ var Dams = {
 		DamParts.Fourth : {
 			"Name" : "Excecuting the master plan",
 			"Price" : {
-				"OakCost" : 1, "AppleCost" : 1, "MapleCost" : 0, "BirchCost" : 0, "SpruceCost" : 0,
+				"OakCost" : 500, "AppleCost" : 500, "MapleCost" : 0, "BirchCost" : 0, "SpruceCost" : 0,
 				"ChestnutCost" : 0, "CherryCost" : 0, "AshCost" : 0, "CedarCost" : 0, "MahoganyCost" : 0,
 				"EbonyCost" : 0, "DogwoodCost" : 0, "RosewoodCost" : 0, "Ghost GumCost" : 0, "DragonwoodCost" : 0,
 			},
